c51d87b03cb5144f78688c01e401780b2eb24a6b
Benjamin Oddou <85166574+BenjaminOddou@users.noreply.github.com>
2023-02-23 10:03:41 +0100

    chore: install.sh
    - Installation script of the Dashlane CLI

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Link to last release : https://api.github.com/repos/Dashlane/dashlane-cli/releases/latest
+# Prefer known version to prevent bugs and breaking changes
+curl -LJO -H 'Accept: application/octet-stream' \
+`curl -s https://api.github.com/repos/Dashlane/dashlane-cli/releases/tags/v1.0.0 \
+| python3 -c 'import sys, json; print([asset["browser_download_url"] for asset in json.load(sys.stdin)["assets"] if "dcli-macos" in asset["name"]][0])'` \
+&& mv dcli-macos dcli \
+&& chmod +x dcli
+
+dirpath=$(dirname $(find /Users/benjaminoddou/Library/Application\ Support/Alfred/Alfred.alfredpreferences/workflows -name '.com.benjamino.dashlane' -type f))
+dirname=$(basename "$dirpath")
+
+if [ -z "$dirpath" ]; then
+    destination="$HOME/Downloads"
+    mv dcli "$destination"
+    echo "🚨 Error ! Dashlane workflow not found, dcli was moved to Downloads folder"
+else
+    destination="/Users/benjaminoddou/Library/Application Support/Alfred/Alfred.alfredpreferences/workflows/$dirname/dcli"
+    mkdir -p "$destination"
+    mv dcli "$destination"
+    echo "✅ Success ! Dashlane CLI is installed correctly"
+fi
+
